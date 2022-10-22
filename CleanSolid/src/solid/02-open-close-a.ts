d66a1e072b72512fde4481f83a37514b9d6780dd
Ruben Contreras <ruben4tip@hotmail.com>
2022-10-22 01:46:52 -0300

    51 listo

--- CleanSolid/src/solid/02-open-close-a.ts
+++ CleanSolid/src/solid/02-open-close-a.ts
@@ -1,11 +1,13 @@
 import { PhotosService, PostService, TodoService } from './02-open-close-b';
+import { HttpClient } from './02-open-close-c';
 
 (async () => {
 
+    const httpClient = new HttpClient();
 
-    const todoService = new TodoService();
-    const postService = new PostService();
-    const photosService = new PhotosService();
+    const todoService = new TodoService(httpClient);
+    const postService = new PostService(httpClient);
+    const photosService = new PhotosService(httpClient);
 
     const todos = await todoService.getTodoItems();
     const posts = await postService.getPosts();
--- CleanSolid/src/solid/02-open-close-b.ts
+++ CleanSolid/src/solid/02-open-close-b.ts
@@ -1,11 +1,14 @@
 // Hay que agregar la dependencia de axios ```yarn add axios```
-import axios from 'axios';
+
+import { HttpClient } from "./02-open-close-c";
 
 
 export class TodoService { 
 
+    constructor ( private http: HttpClient){}
+
     async getTodoItems() {
-        const { data } = await axios.get('https://jsonplaceholder.typicode.com/todos/');
+        const { data } = await this.http.get('https://jsonplaceholder.typicode.com/todos/');
         return data;
     }
 }
@@ -13,8 +16,10 @@ export class TodoService {
 
 export class PostService {
 
+    constructor ( private http: HttpClient){}
+
     async getPosts() {
-        const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        const { data } = await this.http.get('https://jsonplaceholder.typicode.com/posts');
         return data;
     }
 }
@@ -22,8 +27,10 @@ export class PostService {
 
 export class PhotosService {
 
+    constructor ( private http: HttpClient){}
+
     async getPhotos() {
-        const { data } = await axios.get('https://jsonplaceholder.typicode.com/photos');
+        const { data } = await this.http.get('https://jsonplaceholder.typicode.com/photos');
         return data;
     }
 
