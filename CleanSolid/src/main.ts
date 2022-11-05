98ed50e44ea990f3009f402a3b6c3d7962eb851d
Ruben Contreras <ruben4tip@hotmail.com>
2022-11-05 00:02:53 -0300

    ordenado para comenzar

--- CleanSolid/src/main.ts
+++ CleanSolid/src/main.ts
@@ -6,7 +6,8 @@ import './style.css'
 // import './solid/01-srp'
 // import './solid/02-open-close-a'
 // import './solid/02-open-close-a'
-import './solid/03-liskov-a'
+// import './solid/03-liskov-a'
+import './solid/05-dependency-a'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
