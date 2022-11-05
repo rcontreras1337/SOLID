6f493d0e436371810309fc1f6870fb5aecb287df
Ruben Contreras <ruben4tip@hotmail.com>
2022-11-05 00:15:50 -0300

    muestra de problema

--- CleanSolid/src/solid/05-dependency-a.ts
+++ CleanSolid/src/solid/05-dependency-a.ts
@@ -1,10 +1,11 @@
 import { PostService } from './05-dependency-b';
-import { JasonDataBaseService } from './05-dependency-c';
+import { JasonDataBaseService, LocalDataBaseService } from './05-dependency-c';
 
 
 // Main
 (async () => {
-    const provider = new JasonDataBaseService();
+    // const provider = new JasonDataBaseService();
+    const provider = new LocalDataBaseService();
 
     const postService = new PostService( provider );
 
